58b6c827da4e7ba0f4fc1441f146cded461b7356
Ben Firner <ben.firner@gmail.com>
2019-05-06 22:58:42 -0400

    Adding more tests

--- test_attack.py
+++ test_attack.py
@@ -49,6 +49,7 @@ def a_vs_b(ship_a, ship_b, trials, attack_range):
     np_counts = numpy.array(roll_counts)
     return np_counts.mean()
 
+
 def test_brace():
     """Test that brace increases the number of attacks required to destroy a ship."""
 
@@ -66,3 +67,42 @@ def test_brace():
 
         assert(no_brace_attacks < one_brace_attacks)
         assert(one_brace_attacks < two_brace_attacks)
+
+
+def test_scatter():
+    """Test that scatter increases the number of attacks required to destroy a ship."""
+
+    attacker = ship.Ship(name="Attacker", template=ship_templates["Attacker"], upgrades=[], player_number=1)
+
+    no_scatter = ship.Ship(name="No Defense Tokens", template=ship_templates["No Defense Tokens"], upgrades=[], player_number=2)
+    two_scatter = ship.Ship(name="Double Scatter", template=ship_templates["Double Scatter"], upgrades=[], player_number=2)
+
+    # Test with 1000 trials to compensate for the natural variability in rolls
+    for attack_range in ['long', 'medium', 'short']:
+        no_scatter_attacks = a_vs_b(attacker, no_scatter, 1000, attack_range)
+        two_scatter_attacks = a_vs_b(attacker, two_scatter, 1000, attack_range)
+
+        assert(no_scatter_attacks < two_scatter_attacks)
+
+
+def test_evade():
+    """Test that evade increases the number of attacks required to destroy a ship at long or medium range."""
+
+    attacker = ship.Ship(name="Attacker", template=ship_templates["Attacker"], upgrades=[], player_number=1)
+
+    no_evade = ship.Ship(name="No Defense Tokens", template=ship_templates["No Defense Tokens"], upgrades=[], player_number=2)
+    two_evade = ship.Ship(name="Double Evade", template=ship_templates["Double Evade"], upgrades=[], player_number=2)
+
+    # Test with 1000 trials to compensate for the natural variability in rolls
+    no_evade_attacks_long = a_vs_b(attacker, no_evade, 1000, "long")
+    no_evade_attacks_medium = a_vs_b(attacker, no_evade, 1000, "medium")
+    no_evade_attacks_short = a_vs_b(attacker, no_evade, 1000, "short")
+    two_evade_attacks_long = a_vs_b(attacker, two_evade, 1000, "long")
+    two_evade_attacks_medium = a_vs_b(attacker, two_evade, 1000, "medium")
+    two_evade_attacks_short = a_vs_b(attacker, two_evade, 1000, "short")
+
+    # Evades should increase the time to destruction
+    assert(no_evade_attacks_long < two_evade_attacks_long)
+    assert(no_evade_attacks_medium < two_evade_attacks_medium)
+    # Evades do not work at short range
+    assert(pytest.approx(two_evade_attacks_short, 0.1) == no_evade_attacks_short)
